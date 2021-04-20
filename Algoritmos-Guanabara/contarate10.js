80662983c6c6841b8328e6ac9137cf280fcff6d5
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-04-20 17:10:39 -0300

    Exercicios das aulas 9 e 10 sobre estruturas de repeticao

---
+++ Algoritmos-Guanabara/contarate10.js
@@ -0,0 +1,12 @@
+var contador = 0;
+var contador2 = 9;
+
+while (contador <= 10) {
+    console.log(`Estou contando, o valor atual é: ${contador}`);
+    contador++;
+}
+
+while (contador2 >= 0){
+    console.log(`Estou contando, o valor atual é: ${contador2}`);
+    contador2--;
+}
